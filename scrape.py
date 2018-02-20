3dc50d14f65bc4ec4d4d39944b555f164a7db71b
Erica Greene <ericagreene@gmail.com>
2018-02-20 00:35:40 -0500

    Scrape more things

--- scrape.py
+++ scrape.py
@@ -2,15 +2,18 @@
 
 Output:
 
-award, name, location, year, description, website, number, location
+'url', 'year', 'title', 'description', 'address', 'neighborhood', 'phone', 'website'
+
+TODO:
+  * Fix parsing errors
+  * Follow 2nd category pages
+  * Add other themes
 """
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-
-import argparse
 import os.path
 import re
 import requests
@@ -23,11 +26,13 @@ YEAR_URL = 'https://www.bostonmagazine.com/best-of-boston/2017/'
 CATEGORY = 'https://www.bostonmagazine.com/best-of-boston/2017/category/2017-restaurants-food/'
 AWARD_URL = 'https://www.bostonmagazine.com/best-of-boston/2017/award/beer-bar/'
 
+COLUMNS=['url', 'year', 'title', 'description', 'address', 'neighborhood', 'phone', 'website', 'category']
+
 def clean_url():
     pass
 
 def clean_spaces(text):
-    return text.replace('\n', '').replace('\t','')
+    return text.replace('\n', ' ').replace('\t','')
 
 def extract_urls_from_category_page(category_html):
     '''
@@ -48,13 +53,21 @@ def extract_urls_from_category_page(category_html):
 
     return urls
 
-
-
 def extract_meta_from_business_page(business_html):
     soup = BeautifulSoup(business_html, 'html.parser')
 
     # url
     url = soup.findAll('link', {'rel': 'canonical'})[0]['href']
+
+    # Check for next page URLs, then don't try to parse. For example,
+    # '/best-of-boston/2017/award/new-restaurant/page/2/'
+    next_page_regex = re.compile('.*/page/[0-9]+/*')
+    if re.match(next_page_regex, url) != None:
+        return
+
+    # category
+    category = soup.findAll('h2', {'class': 'thin-line'})[0].text
+    category = ' '.join(clean_spaces(category).split(' ')[1:])
 
     # year
     year = url.split('/')[-3] # HACK, use a regex
@@ -77,6 +90,7 @@ def extract_meta_from_business_page(business_html):
         'url': url,
         'year': year,
         'title': title,
+        'category': category,
         'description': description,
         'address': address,
         'neighborhood': neighborhood,
@@ -99,11 +113,14 @@ def scrape_if_necessary(url, type):
         print('type supplied to scrape_if_necessary not valid')
         raise ValueError()
 
-    path = 'data/{0}_{1}.html'.format(type, url.split('/')[-2])
+    if type == 'category':
+        path = 'data/{0}_{1}_{2}.html'.format(type, url.split('/')[-1], url.split('/')[-2])
+    else:
+        path = 'data/{0}_{1}.html'.format(type, url.split('/')[-2])
 
     # If we already have the HTML for the page, don't scrape it again
     if os.path.isfile(path):
-        print('skipping {}'.format(path))
+        #print('skipping {}'.format(path))
         return path
 
     print('scraping', path)
@@ -116,10 +133,13 @@ def scrape_if_necessary(url, type):
 
     return path
 
-def main():
-
-    beer_bar_html = open('data/category_beer-bar.html', 'r')
-    urls = extract_urls_from_category_page(beer_bar_html)
+def extract_data_from_category(category_html):
+    '''
+    Given HTML from a category page like (e.g. /best-of-boston/2017/award/beer-bar/)
+    returns a DataFrame with the information from all the winners in that category
+    over the years.
+    '''
+    urls = extract_urls_from_category_page(category_html)
 
     # Scrape pages for all URLs and save HTML files to disk
     paths = []
@@ -128,23 +148,48 @@ def main():
         paths.append(path)
 
     # Read all business HTML files from disk and extra data
-    columns=['url', 'year', 'title', 'description', 'address', 'neighborhood', 'phone', 'website']
-    df = pd.DataFrame(columns=columns)
+
+    df_category = pd.DataFrame(columns=COLUMNS)
 
     for p in paths:
         business_html = open(p, 'r')
 
         try:
             meta = extract_meta_from_business_page(business_html)
-            df_business = pd.DataFrame(data=meta, columns=columns, index=[0])
-            df = df.append(df_business)
+            print(meta['url'])
+
+            df_business = pd.DataFrame(data=meta, columns=COLUMNS, index=[0])
+            df_category = df_category.append(df_business)
         except:
-            print('unable to parse {0}'.format(p))
+            print('Error: unable to parse {0}'.format(p))
 
 
     # Write parsed data to CSV
-    df.to_csv('bob_data.csv', encoding='utf-8')
-    return
+    return df_category
+
+def main():
+    restaurant_html = open('data/theme_restaurants.html', 'r')
+    soup = BeautifulSoup(restaurant_html, 'html.parser')
+
+    # Extract all URLs for category pages, e.g. list of Best Beer Bars
+    category_url_regex = re.compile('.*/best-of-boston/[0-9]+/award/.*')
+    category_urls = [l['href'] for l in soup.findAll('a',  href=category_url_regex)]
+    category_urls = list(set(category_urls))
+
+    # Scrape all category pages
+    theme_paths = []
+    for url in category_urls:
+        path = scrape_if_necessary(url, 'category')
+        theme_paths.append(path)
+
+    df_theme = pd.DataFrame(columns=COLUMNS)
+
+    for path in theme_paths:
+        category_html = open(path, 'r')
+        df_category = extract_data_from_category(category_html)
+        df_theme = df_theme.append(df_category)
+
+    df_theme.to_csv('bob_data.csv', encoding='utf-8')
 
 if __name__ == "__main__":
     main()
